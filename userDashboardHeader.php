074250638c2b65692a4871312c2f17f843e5d78b
aakash chandhoke <aakash chandhoke>
2018-04-07 19:35:29 +0530

    Minor changes in userDashBoard Header remove settings from logout section

--- userDashboardHeader.php
+++ userDashboardHeader.php
@@ -103,9 +103,11 @@
                 <div class="col-sm-5">
                     <div class="user-area dropdown float-right"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <img class="user-avatar rounded-circle" src="images/admin.jpg" alt="User Avatar"> </a>
                         <div class="user-menu dropdown-menu"> 
-							<a class="nav-link" href="#"><i class="fa fa- user"></i>My Profile</a> 
+							<a class="nav-link" href="userindex.php"><i class="fa fa- user"></i>My Profile</a> 
+<!--
 							<a class="nav-link" href="#"><i class="fa fa- user"></i>Notifications <span class="count">13</span></a> 
 							<a class="nav-link" href="#"><i class="fa fa -cog"></i>Settings</a>
+-->
 							<a class="nav-link" href="logout.php"><i class="fa fa-power -off"></i>Logout</a> </div>
                     </div>
                 </div>
