8e5c7f4b8200d8ed3a58ce9968193d07b77bc1ff
aakashchandhoke <aakash.chandhoke24@gmail.com>
2018-04-09 00:59:00 +0530

    Added Pix in the $squareApiSection

--- adminindex4sTest.php
+++ adminindex4sTest.php
@@ -1,8 +1,6 @@
-<?php include( "adminDashboardHeader.php" );
+<?php include( "userDashboardHeader.php" );
 $client_key="JPBEZAJ1STTXOB0BLE5FQTA0A421K2HMBA1IHKDLGPDPDHCM";
 $client_secret = "OFLMCJQZYOZXMLDAAMD4K1SP20LH0D0H4DB3F4BU4I5YRDR1";
-//$client_key = "JVAH23FK35WYAJQRX3QHKPH5PYKQGFAPFXPRJWTGDT0HWVBT";
-//$client_secret = "I5KV04QHLSMJJUIZ0BKYOV4UZK3PVZ4BJFIGR2GGCU003WEQ";
 function callGetApi($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -45,7 +43,7 @@ function getPhotoSingle($venueID) {
         $output = callGetApi($url);
 
         if ($output['response']['photos']['count'] >= 1) {
-            $photos[0] = $output['response']['photos']['items'][0]['prefix'] . '500x500' . $output['response']['photos']['items'][0]['suffix'];
+            $photos[0] = $output['response']['photos']['items'][0]['prefix'] . '400x300' . $output['response']['photos']['items'][0]['suffix'];
 
             //echo "url = ".$venueList[$i]['photo']."\n";
         } else {
@@ -101,65 +99,35 @@ function getVenuesListUsingLatLng($lat, $lng, $radius = 20000) {
 
 ?>
 
-<div class="breadcrumbs">
-    <div class="col-sm-4">
-        <div class="page-header float-left">
-            <div class="page-title">
-                <h1>Dashboard</h1>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="content mt-3">
-    <div class="col-sm-12">
-        <div class="alert  alert-success alert-dismissible fade show" role="alert"> <span class="badge badge-pill badge-success">Hello</span> Welcome to the AITG Dashboard.
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
-        </div>
-    </div>
-
-    <!--/.col-->
-
-    <div class="col-lg-3 col-md-6">
-        <div class="social-box facebook"> <i class="fa fa-facebook"></i> </div>
-    </div>
-    <!--/.col-->
-
-    <div class="col-lg-3 col-md-6">
-        <div class="social-box twitter"> <i class="fa fa-twitter"></i> </div>
-        <!--/social-box-->
-    </div>
-    <!--/.col-->
-
-    <div class="col-lg-3 col-md-6">
-        <div class="social-box linkedin"> <i class="fa fa-linkedin"></i> </div>
-        <!--/social-box-->
-    </div>
-    <!--/.col-->
     
     <?php
         
-        $resultFrom4s = getVenuesListUsingLatLng(25.4358, 81.8463, 20000);
+        $resultFrom4s = getVenuesListUsingLatLng(41.9028, 12.4964, 20000);
         foreach($resultFrom4s as $i) {
             //$resultFromPhotos = getPhotos($i['id']);
             //echo($resultFromPhotos);
-            echo ("         <div class='col-xl-6 col-lg-6'>
+            echo ("         <div class='col-xl-4 col-lg-3'>
         <div class='card'>
             <div class='card-body'>
                 <div class='stat-widget-one'>
-                    <div class='stat-icon dib'><i class='ti-user text-primary border-primary'></i>
-                    
-                    </div>
+                    <div>");
+                    echo ("         <div class='stat-text '><h6><strong>" . $i['name'] . "<strong></h6></div><br>");
+                    echo ("         <div><img alt='image' src='" . getPhotoSingle($i['id']) . "'/></div><br/>");
+                    echo("</div>
                     <div class='stat-content dib'>");
-            echo ("         <div class='stat-text'>" . $i['id'] . "</div>");
-            echo ("         <div class='stat-text'>" . $i['name'] . "</div>");
-            //echo ("           <div class='stat-text'>" . $i['photos'] . "</div>");
-            if(isset($i['address']))
+            if(isset($i['address'])){
+                echo ("         <div class='stat-text '><strong>Address : </strong></div>");
                 $add = $i["address"];
+            }
+                
             else
-                $add = "";
-            echo ("         <div class='stat-text'>" . $add ."</div>");
-            echo ("         <div class='stat-text'>" . $i['description'] . "</div>");
-            echo ("         <div><img alt='image' src='" . getPhotoSingle($i['id']) . "'/></div>");
+                $add = "<br>";
+           
+          
+            
+             echo ("         <div class='stat-text'><mark>" . $add ."<mark></div>");
+            echo ("         <div class='stat-text '><strong> Ratings : </strong></div>");
+             echo ("         <div class='stat-text'><mark>" . $i['rating'] . "<mark></div>");
             echo ("
                         
                     </div>
@@ -177,41 +145,6 @@ function getVenuesListUsingLatLng($lat, $lng, $radius = 20000) {
     ?>
     
     
-    <div class="col-xl-3 col-lg-6">
-        <div class="card">
-            <div class="card-body">
-                <div class="stat-widget-one">
-                    <div class="stat-icon dib"><i class="ti-layout-grid2 text-warning border-warning"></i>
-                    </div>
-                    <div class="stat-content dib">
-                        <div class="stat-text">Places</div>
-                        <div class="stat-digit">5</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-6">
-        <div class="card">
-            <div class="card-header">
-                <h4>India</h4>
-                <div id="googleMap" style="width:100%;height:400px;"></div>
-                <script>
-                    function myMap() {
-                        var mapProp = {
-                            center: new google.maps.LatLng( 25.4920, 81.8639 ),
-                            zoom: 15,
-                        };
-                        var map = new google.maps.Map( document.getElementById( "googleMap" ), mapProp );
-                    }
-                </script>
-                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDxYuQle47vALGQKq5P_8fJYXHQmZEWSo4&callback=myMap"></script>
-            </div>
-        </div>
-        <!-- /# card -->
-    </div>
-</div>
-<!-- .content --> 
-</div>
+    
 <!-- /#right-panel --> 
-<?php include( "adminDashboardFooter.php" ); ?>
+<?php include( "userDashboardFooter.php" ); ?>
