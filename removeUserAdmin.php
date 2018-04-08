cf03f240445ff8a7cce917cba9b879a485a880c0
Shubham Kumar <shubham.kumar.sci@gmail.com>
2018-04-08 16:31:38 +0530

    AddRemove Tags Working

--- removeUserAdmin.php
+++ removeUserAdmin.php
@@ -1,34 +1,63 @@
-<?php include( "adminDashboardHeader.php" ); ?>
+<?php
+include( "queryFunctions.php" );
+if ( isset( $_POST[ "btnremove" ] ) ) {
+	if ( removeUser( $_POST[ "cbuser" ] ) ) {
+		header( "Location: removeUserAdmin.php?status=UserRemoved" );
+	} else {
+		header( "Location: removeUserAdmin.php?status=SomethingWentWrong" );
+	}
+
+}
+include( "adminDashboardHeader.php" );
+?>
+
 <div class="breadcrumbs">
-    <div class="col-sm-4">
-        <div class="page-header float-left">
-            <div class="page-title">
-                <h1>Dashboard</h1>
-            </div>
-        </div>
-    </div>
+	<div class="col-sm-4">
+		<div class="page-header float-left">
+			<div class="page-title">
+				<h1>
+				
+				<?php 
+					if (isset($_GET["status"])) {
+						//var_dump($_GET);
+						echo $_GET["status"];
+					}
+					?>
+				</h1>
+			</div>
+		</div>
+	</div>
 </div>
 <div class="content mt-3">
-    <div class="col-lg-12">
-        <div class="card">
-            <div class="card-header"><strong>Remove User</strong>
-            </div>
-            <div class="card-body card-block">
-                <div class="form-group">
-                    <div class="row form-group">
-                    <div class="col-md-4">
-                    <label for="vat">Username</label>
-                    <select name="cbuser" id="activities" class="form-control">
-                        <option value="">Admin</option>
-                    </select>
-                </div>
-            </div>
-                    <label for="vat" class=" form-control-label">Faltu INfo</label><br>
-                    <button id="payment-button" type="submit" name="btnremove" class="btn btn-lg btn-info col-lg-2"> <i class="fa fa-lg"></i>&nbsp; <span id="payment-button-amount">Remove</span> <span id="payment-button-sending" style="display:none;">Sending…</span> </button>
-                </div>
-            </div>
-        </div>
-        <!-- .content -->
-    </div>
+	<div class="col-lg-12">
+		<div class="card">
+			<div class="card-header"><strong>Remove User</strong>
+			</div>
+			<div class="card-body card-block">
+				<form method="post" action="removeUserAdmin.php">
+					<div class="form-group">
+						<div class="row form-group">
+							<div class="col-md-4">
+								<label for="vat">Username</label>
+								<select name="cbuser" id="activities" class="form-control">
+									<?php
+									global $connection;
+									$query = "SELECT * FROM users";
+									$results = mysqli_query( $connection, $query );
+									while ( $row = mysqli_fetch_assoc( $results ) ) {
+										echo( "<option value = '" . $row[ "userid" ] . "' >" . $row[ "username" ] . " (" . $row[ type ] . ") </option>" );
+									}
+									?>
+								</select>
+							</div>
+						</div>
+<!--						<label for="vat" class=" form-control-label">Faltu INfo</label><br>-->
+						<button id="payment-button" type="submit" name="btnremove" class="btn btn-lg btn-info col-lg-2"> <i class="fa fa-lg"></i>&nbsp; <span id="payment-button-amount">Remove</span> <span id="payment-button-sending" style="display:none;">Sending…</span> </button>
+					</div>
+				</form>
+			</div>
+		</div>
+		<!-- .content -->
+	</div>
 </div>
 <?php include( "adminDashboardFooter.php" ); ?>
--- tags.php
+++ tags.php
@@ -1,40 +1,75 @@
-<?php include( "adminDashboardHeader.php" ); ?>
+<?php
+include( "queryFunctions.php" );
+if ( isset( $_POST[ "btnremove" ] ) ) {
+	if ( removeTag( $_POST[ "cbtags" ] ) ) {
+		header( "Location: tags.php?status=TagRemoved" );
+	} else {
+		header( "Location: tags.php?status=SomethingWentWrong" );
+	}
+
+}
+if ( isset( $_POST[ "btnadd" ] ) ) {
+	if ( addTag( $_POST[ "tbtag" ] ) ) {
+		header( "Location: tags.php?status=TagAdded" );
+	} else {
+		header( "Location: tags.php?status=SomethingWentWrong" );
+	}
+
+}
+include( "adminDashboardHeader.php" );
+?>
 <div class="breadcrumbs">
-    <div class="col-sm-4">
-        <div class="page-header float-left">
-            <div class="page-title">
-                <h1>Dashboard</h1>
-            </div>
-        </div>
-    </div>
+	<div class="col-sm-4">
+		<div class="page-header float-left">
+			<div class="page-title">
+				<h1>
+					<?php 
+					if (isset($_GET["status"])) {
+						//var_dump($_GET);
+						echo $_GET["status"];
+					}
+					?>
+				</h1>
+			</div>
+		</div>
+	</div>
 </div>
 <div class="content mt-3">
-    <div class="col-lg-12">
-        <div class="card">
-            <div class="card-header"><strong>Add or Remove Tag</strong>
-            </div>
-            <div class="card-body card-block">
-                <div class="form-group">
-                    <label for="vat" class=" form-control-label">Add Tags</label>
-                    <input type="text" id="vat" placeholder="Enter tag" name="tbname" class="form-control col-lg-12">
-                    <br>
-                    <button id="payment-button" type="submit" name="btnadd" class="btn btn-lg btn-info col-lg-2"> <i class="fa fa-lg"></i>&nbsp; <span id="payment-button-amount">Add</span> <span id="payment-button-sending" style="display:none;">Sending…</span> </button>
-                    
-                    <br/>
-                    <br/>
-                    <div class="row form-group">
-                    <div class="col-md-4">
-                    <label for="vat">Remove Tag</label>
-                    <select name="cbtags" id="activities" class="form-control">
-                        <option value="">1</option>
-                    </select>
-                </div>
-            </div>
-                    <button id="payment-button" type="submit" name="btnremove" class="btn btn-lg btn-info col-lg-2"> <i class="fa fa-lg"></i>&nbsp; <span id="payment-button-amount">Remove</span> <span id="payment-button-sending" style="display:none;">Sending…</span> </button>
-                </div>
-            </div>
-        </div>
-        <!-- .content -->
-    </div>
+	<div class="col-lg-12">
+		<div class="card">
+			<div class="card-header"><strong>Add or Remove Tag</strong>
+			</div>
+			<div class="card-body card-block">
+				<form method="post" action="tags.php">
+					<div class="form-group">
+						<label for="vat" class=" form-control-label">Add Tags</label>
+						<input type="text" id="vat" placeholder="Enter tag" name="tbtag" class="form-control col-lg-12">
+						<br>
+						<button id="payment-button" type="submit" name="btnadd" class="btn btn-lg btn-info col-lg-2"> <i class="fa fa-lg"></i>&nbsp; <span id="payment-button-amount">Add</span> <span id="payment-button-sending" style="display:none;">Sending…</span> </button>
+
+						<br/>
+						<br/>
+						<div class="row form-group">
+							<div class="col-md-4">
+								<label for="vat">Remove Tag</label>
+								<select name="cbtags" id="activities" class="form-control">
+									<?php
+									global $connection;
+									$query = "SELECT * FROM tags";
+									$results = mysqli_query( $connection, $query );
+									while ( $row = mysqli_fetch_assoc( $results ) ) {
+										echo( "<option value = '" . $row[ "tagid" ] . "' >" . $row[ "tagname" ] . " </option>" );
+									}
+									?>
+								</select>
+							</div>
+						</div>
+						<button id="payment-button" type="submit" name="btnremove" class="btn btn-lg btn-info col-lg-2"> <i class="fa fa-lg"></i>&nbsp; <span id="payment-button-amount">Remove</span> <span id="payment-button-sending" style="display:none;">Sending…</span> </button>
+					</div>
+				</form>
+			</div>
+		</div>
+		<!-- .content -->
+	</div>
 </div>
 <?php include( "adminDashboardFooter.php" ); ?>
