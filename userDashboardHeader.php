27bc31c6b91b38d7cac1e8dac222b7b4725efed7
aakashchandhoke <aakash.chandhoke24@gmail.com>
2018-04-09 01:34:48 +0530

    Changed drop down list from header user

--- userDashboardHeader.php
+++ userDashboardHeader.php
@@ -89,18 +89,18 @@
         <!-- Header-->
         <header id="header" class="header">
             <div class="header-menu">
-                <div class="col-sm-7"> <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
-                    <div class="header-left">
-                        <button class="search-trigger"><i class="fa fa-search"></i></button>
-                        <div class="form-inline">
-                            <form class="search-form">
-                                <input class="form-control mr-sm-2" type="text" placeholder="Search ..." aria-label="Search">
-                                <button class="search-close" type="submit"><i class="fa fa-close"></i></button>
-                            </form>
-                        </div>
+                <div class="col-sm-10"> <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
+                    <div class="col-sm-6">
+                        <select name="cbplaces" id="activities" class="form-control">
+                        <option value="">Italy</option>
+                    </select>
+                    </div>
+                    <div class="col-sm-4">
+                    <button id="payment-button" type="submit" name="btnadd" class="btn btn-md btn-info">Add</button>
                     </div>
                 </div>
-                <div class="col-sm-5">
+                
+                <div class="col-sm-2">
                     <div class="user-area dropdown float-right"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <img class="user-avatar rounded-circle" src="images/admin.jpg" alt="User Avatar"> </a>
                         <div class="user-menu dropdown-menu"> 
 							<a class="nav-link" href="userindex.php"><i class="fa fa- user"></i>My Profile</a> 
