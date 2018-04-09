d6b36fd4f349d4a282703936dcb0f8ada49694fc
aakashchandhoke <aakash.chandhoke24@gmail.com>
2018-04-10 00:25:50 +0530

    Changes in userindex id mentioning

--- userDashboardHeader.php
+++ userDashboardHeader.php
@@ -53,18 +53,6 @@ if ( mysqli_connect_errno() ) {
 			<div id="main-menu" class="main-menu collapse navbar-collapse">
 				<ul class="nav navbar-nav">
 					<li class="active"> <a href="userindex.php"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a> </li>
-					<!--<li class="menu-item-has-children dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Travel</a>
-                        <ul class="sub-menu children dropdown-menu">
-                            <li><i class="fa fa-user"></i><a href="userapi.php">Location</a>
-                            </li>
-                            <li><i class="fa fa-user"></i><a href="userapi.php">Restaurants</a>
-                            </li>
-                            <li><i class="fa fa-user"></i><a href="userapi.php">Hospitals</a>
-                            </li>
-                            <li><i class="fa fa-user"></i><a href="userapi.php">Bars</a>
-                            </li>
-                        </ul>
-                    </li>-->
 					<h3 class="menu-title">Manage</h3>
 					<!-- /.menu-title -->
 					<li class="menu-item-has-children dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-align-justify"></i>Preferences</a>
